13b575655d8a0e91d706eb5fe3c3f2c348bad62f
karinEkenberg <zirkon6@hotmail.com>
2024-01-03 10:06:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ Uppgift: Hantera en inköpslista
+1. Skapa en klass Inköpslista som innehåller en lista av produkter (List<string>
+produkter).
+2. Skapa en metod LäggTillProdukt som tar emot en produkt och lägger till den
+i listan.
+3. Skapa en metod TaBortProdukt som tar emot en produkt och tar bort den
+från listan om den finns.
+4. Skapa en metod SkrivUtInköpslista som skriver ut alla produkter i
+inköpslistan.
+ */
+namespace classExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
